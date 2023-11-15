1a8ced4df8edde5575e841b82f5b34c9730352fe
Guilherme Monção <guigagm27@gmail.com>
2023-11-15 19:56:27 -0300

    feat: Code eslint style config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from "express"
 
 const server = express()
 
-server.get('/', (_, res) => {
-    return res.send('Tudo certo')
+server.get("/", (_, res) => {
+	return res.send("Tudo certo")
 })
 
 export { server }
